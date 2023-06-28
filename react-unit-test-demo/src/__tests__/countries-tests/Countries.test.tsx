b578e96cc36c9b80fc8d74e5342d574370acad6e
Oussama Rahmouni <ORahmouni@selectmedical.com>
2023-06-27 23:36:13 -0400

    Remove unused import

--- react-unit-test-demo/src/__tests__/countries-tests/Countries.test.tsx
+++ react-unit-test-demo/src/__tests__/countries-tests/Countries.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Countries, { CountryName } from "../../components/countries/Countries";
 import { server } from "../../testServer";
 import { rest } from "msw";
